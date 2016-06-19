905decf4f9f935ba026998a3147d0371771b4dd1
Landry Breuil <landry@xfce.org>
2016-06-19 15:08:21 +0200

    Fix progressbar css styling with Gtk 3.20, and set min-width/height to 4px while here

--- panel-plugin/battery.c
+++ panel-plugin/battery.c
@@ -600,7 +600,13 @@ battmon.c:241: for each function it appears in.)
     }
 
 #if GTK_CHECK_VERSION (3, 16, 0)
+#if GTK_CHECK_VERSION (3, 20, 0)
+    gchar *css = g_strdup_printf("progressbar trough { min-width: 4px; min-height: 4px; } \
+                                  progressbar progress { min-width: 4px; min-height: 4px; \
+                                                         background-color: %s; background-image: none; }",
+#else
     gchar *css = g_strdup_printf("progressbar progress { background-color: %s; background-image: none; }",
+#endif
                                  gdk_rgba_to_string(color));
     GtkCssProvider *css_provider = gtk_css_provider_new ();
     gtk_css_provider_load_from_data (css_provider, css, strlen(css), NULL);

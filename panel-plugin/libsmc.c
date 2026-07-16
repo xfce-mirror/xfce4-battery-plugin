xfce-mirror/xfce4-battery-plugin

commit 01073694beee22a170c4cf6c451d7b69dab5f793

cppcheck: Fix unreadVariable

panel-plugin/libsmc.c
@@ -130,22 +130,18 @@ __attribute__((destructor)) void smc_close(void) {
 }
 
 int get_fan_status(void) {
-  IOReturn result = kIOReturnSuccess;
   SMCKey key;
   char keyStr[5];
   uint8_t fan_num;
   int i;
 
-  if (gConn == 0)
-    result = smc_open();
-
-  if (result != kIOReturnSuccess)
+  if (gConn == 0 && smc_open() != kIOReturnSuccess)
     return 0;
 
   key = makeUInt32Key("FNum", 4, 16);
   result = smc_read(key, &fan_num);
   /* No hardware fan support, or permission deined */
-  if (result != kIOReturnSuccess)
+  if (smc_read(key, &fan_num) != kIOReturnSuccess)
     return 0;
 
   /* FNum(ui8) = 0, no fans on device */
@@ -158,7 +154,7 @@ int get_fan_status(void) {
 
     sprintf(keyStr, "F%dAc", i);
     key = makeUInt32Key(keyStr, 4, 16);
-    result = smc_read(key, &retval);
+    smc_read(key, &retval);
     /* F*Ac(flt), return 1 if any fan working */
     if (retval > 0.0)
       return 1;

panel-plugin/libacpi.c
@@ -275,8 +275,6 @@ check_acpi(void)
     static char buf1[BUFSIZ];
     char *bufp=buf1;
     char fmt[BUFSIZ];
-    void *oldp=(void *)buf1;
-    size_t oldlenp=BUFSIZ;
     int len,mib[CTL_MAXNAME];
     u_int kind;
     snprintf(buf1, BUFSIZ, "%s", "hw.acpi.battery.units");
@@ -367,9 +365,7 @@ read_acad_state(void)
 #ifdef HAVE_SYSCTL
     static char buf1[BUFSIZ];
     char fmt[BUFSIZ];
-    void *oldp=(void *)buf1;
     char *bufp=buf1;
-    size_t oldlenp=BUFSIZ;
     int len,mib[CTL_MAXNAME];
     u_int kind;
     int retval;
@@ -595,8 +591,6 @@ read_acpi_state(int battery)
     static char buf1[BUFSIZ];
     char fmt[BUFSIZ];
     char *bufp=buf1;
-    void *oldp=(void *)buf1;
-    size_t oldlenp=BUFSIZ;
     int len,mib[CTL_MAXNAME];
     int retval;
     u_int kind;
@@ -719,8 +713,6 @@ get_temperature(void)
     static char buf1[BUFSIZ];
     char fmt[BUFSIZ];
     char *bufp=buf1;
-    void *oldp=(void *)buf1;
-    size_t oldlenp=BUFSIZ;
     int len,mib[CTL_MAXNAME];
     int retval;
     u_int kind;

commit c12ffd3e74aba549e6670b2ef4987efba692a1c2

Remove extra call to smc_read
Fixes: 01073694beee22a170c4cf6c451d7b69dab5f793

panel-plugin/libsmc.c
@@ -139,7 +139,6 @@ int get_fan_status(void) {
     return 0;
 
   key = makeUInt32Key("FNum", 4, 16);
-  result = smc_read(key, &fan_num);
   /* No hardware fan support, or permission deined */
   if (smc_read(key, &fan_num) != kIOReturnSuccess)
     return 0;